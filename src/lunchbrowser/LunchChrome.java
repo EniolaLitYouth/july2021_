02ee6a696ce3126c6800ffb7da7bcc193e1722c0
EniolaLitYouth <37642579+EniolaLitYouth@users.noreply.github.com>
2021-11-20 18:50:14 -0600

    first commit

---
+++ src/lunchbrowser/LunchChrome.java
@@ -0,0 +1,26 @@
+package lunchbrowser;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class LunchChrome {
+
+	public static void main(String[] args) {
+	
+ 
+		System.setProperty("webdriver.chrome.driver ","C:\\Users\\Owner\\july2021_Selelium\\FirstSelenium\\driver\\chromedriver.exe ");
+		WebDriver driver = new ChromeDriver();
+	
+	
+		
+  
+
+	
+	
+	}
+
+}
